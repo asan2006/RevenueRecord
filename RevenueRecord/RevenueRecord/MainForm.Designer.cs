b1394b790879f59d323f23625c6d1158c6305382
a <san>
2014-02-09 22:18:29 +0800

    Revenue record project
    VS2012 + c# + .NET 4.0
    part UI design

---
+++ RevenueRecord/RevenueRecord/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace RevenueRecord
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpData = new System.Windows.Forms.TabPage();
+            this.tpChart = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tpUser = new System.Windows.Forms.TabPage();
+            this.tpAuthor = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tpData.SuspendLayout();
+            this.tpChart.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpData);
+            this.tabControl1.Controls.Add(this.tpChart);
+            this.tabControl1.Controls.Add(this.tpUser);
+            this.tabControl1.Controls.Add(this.tpAuthor);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tabControl1.ItemSize = new System.Drawing.Size(96, 30);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(523, 403);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tpData
+            // 
+            this.tpData.Controls.Add(this.textBox1);
+            this.tpData.Controls.Add(this.dateTimePicker2);
+            this.tpData.Controls.Add(this.label4);
+            this.tpData.Controls.Add(this.label3);
+            this.tpData.Controls.Add(this.label2);
+            this.tpData.Controls.Add(this.label1);
+            this.tpData.Controls.Add(this.button1);
+            this.tpData.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tpData.Location = new System.Drawing.Point(4, 34);
+            this.tpData.Name = "tpData";
+            this.tpData.Padding = new System.Windows.Forms.Padding(3);
+            this.tpData.Size = new System.Drawing.Size(515, 365);
+            this.tpData.TabIndex = 0;
+            this.tpData.Text = "数据管理";
+            this.tpData.UseVisualStyleBackColor = true;
+            // 
+            // tpChart
+            // 
+            this.tpChart.Controls.Add(this.label5);
+            this.tpChart.Controls.Add(this.dateTimePicker3);
+            this.tpChart.Controls.Add(this.dateTimePicker1);
+            this.tpChart.Location = new System.Drawing.Point(4, 34);
+            this.tpChart.Name = "tpChart";
+            this.tpChart.Padding = new System.Windows.Forms.Padding(3);
+            this.tpChart.Size = new System.Drawing.Size(515, 365);
+            this.tpChart.TabIndex = 1;
+            this.tpChart.Text = "曲线图";
+            this.tpChart.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tpUser
+            // 
+            this.tpUser.Location = new System.Drawing.Point(4, 34);
+            this.tpUser.Name = "tpUser";
+            this.tpUser.Padding = new System.Windows.Forms.Padding(3);
+            this.tpUser.Size = new System.Drawing.Size(515, 365);
+            this.tpUser.TabIndex = 2;
+            this.tpUser.Text = "用户信息";
+            this.tpUser.UseVisualStyleBackColor = true;
+            // 
+            // tpAuthor
+            // 
+            this.tpAuthor.Location = new System.Drawing.Point(4, 34);
+            this.tpAuthor.Name = "tpAuthor";
+            this.tpAuthor.Padding = new System.Windows.Forms.Padding(3);
+            this.tpAuthor.Size = new System.Drawing.Size(515, 365);
+            this.tpAuthor.TabIndex = 3;
+            this.tpAuthor.Text = "关于软件";
+            this.tpAuthor.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "这里显示日期，包含农历";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "这里显示地址和天气信息";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(8, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "营业额：";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(103, 134);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(309, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 30);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "000000.00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(423, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "元";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(8, 6);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(249, 6);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePicker3.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(214, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "到";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 403);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "营业额记录";
+            this.tabControl1.ResumeLayout(false);
+            this.tpData.ResumeLayout(false);
+            this.tpData.PerformLayout();
+            this.tpChart.ResumeLayout(false);
+            this.tpChart.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpData;
+        private System.Windows.Forms.TabPage tpChart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tpUser;
+        private System.Windows.Forms.TabPage tpAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+
+
+    }
+}
+
